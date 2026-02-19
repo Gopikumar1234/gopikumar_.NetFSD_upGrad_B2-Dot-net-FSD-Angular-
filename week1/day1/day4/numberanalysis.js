e89d2c1c400dab850489c8c571b7fd335223a3a1
Gopikumar1234 <nandigopikumar@gmail.com>
2026-02-19 15:16:00 +0530

    Add files via upload

---
+++ week1/day1/day4/numberanalysis.js
@@ -0,0 +1,17 @@
+let num = 7;
+
+// Positive or Negative using ternary
+let result = (num >= 0) ? "Positive" : "Negative";
+console.log("Number is: " + result);
+
+// Even or Odd using if-else
+if (num % 2 === 0) {
+  console.log("Even Number");
+} else {
+  console.log("Odd Number");
+}
+
+// Loop from 1 to num
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
