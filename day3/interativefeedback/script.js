3184e10a3fc20d8a3875d510d0611880db2c60cb
Gopikumar1234 <nandigopikumar@gmail.com>
2026-02-27 13:00:51 +0530

    Delete day3 directory

--- day3/interativefeedback/script.js
+++
@@ -1,8 +0,0 @@
-// Select elements
-const feedbackBtn = document.getElementById("feedbackBtn");
-const messageDiv = document.getElementById("message");
-
-// Add event listener
-feedbackBtn.addEventListener("click", function () {
-    messageDiv.textContent = "✅ Thank you! Your feedback has been submitted.";
-});
